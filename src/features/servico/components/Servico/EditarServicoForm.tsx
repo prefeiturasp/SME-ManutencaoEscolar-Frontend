0f18a994636ede70c5505c6707ff484ca5cc5105
Matheus Bonaretti <bona.matheuss2@gmail.com>
2026-08-13 17:27:54 -0300

    feat(151775): ajustado o texto do retorno

--- src/features/servico/components/Servico/EditarServicoForm.tsx
+++ src/features/servico/components/Servico/EditarServicoForm.tsx
@@ -161,8 +161,8 @@ export function EditarServicoForm({ uuid, servico }: EditarServicoFormProps) {
 
           <Card className="p-6">
             <CardTitle className="text-sm text-muted-foreground">
-              Atualize as informações e clique em “salvar alterações” para
-              armazenar os dados.
+              Preencha as informações e clique em “salvar” para armazenar os
+              dados.
             </CardTitle>
 
             <FormServico />
