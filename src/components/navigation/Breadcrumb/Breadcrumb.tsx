8ec327126dd3f26fbb57486bc850b35475351dd0
brunosp1024 <brunosalvador145@gmail.com>
2026-08-14 09:12:23 -0300

    refactor (154698): remove links do breadcrumbs, mantendo apenas o link para o inicio

--- src/components/navigation/Breadcrumb/Breadcrumb.tsx
+++ src/components/navigation/Breadcrumb/Breadcrumb.tsx
@@ -3,7 +3,6 @@ import { cn } from "@/lib/utils";
 import { BreadcrumbSeparator } from "../Breadcrumb/BreadcrumbSeparator";
 import { BreadcrumbItem } from "./BreadcrumbItem";
 import type { PropriedadesBreadcrumb } from "./types/Breadcrumb.types";
-import Link from "next/link";
 
 export function Breadcrumb({ itens, className }: PropriedadesBreadcrumb) {
   if (itens.length === 0) {
@@ -15,10 +14,6 @@ export function Breadcrumb({ itens, className }: PropriedadesBreadcrumb) {
       <ol className="flex items-center gap-3 text-xs">
         {itens.map((item, indice) => {
           const ultimoItem = indice === itens.length - 1;
-          const itemSeguinte = ultimoItem ? null : itens[indice + 1];
-          const itemSeguinteEhUltimo = itemSeguinte
-            ? indice + 1 === itens.length - 1
-            : false;
 
           return (
             <li
@@ -27,16 +22,7 @@ export function Breadcrumb({ itens, className }: PropriedadesBreadcrumb) {
             >
               <BreadcrumbItem item={item} />
 
-              {!ultimoItem && itemSeguinte && !itemSeguinteEhUltimo ? (
-                <Link
-                  href={String(itemSeguinte.caminho)}
-                  className="flex items-center gap-2"
-                >
-                  <BreadcrumbSeparator />
-                </Link>
-              ) : (
-                !ultimoItem && <BreadcrumbSeparator />
-              )}
+              {!ultimoItem && <BreadcrumbSeparator />}
             </li>
           );
         })}
