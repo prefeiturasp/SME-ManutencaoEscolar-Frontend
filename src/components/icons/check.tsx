86ccd855f4e621e4fd6d8615fe25da8a18ea004d
brunosp1024 <brunosalvador145@gmail.com>
2026-08-28 17:01:29 -0300

    style (154959): atualiza classes de estilos em componentes globais

--- src/components/icons/check.tsx
+++ src/components/icons/check.tsx
@@ -1,13 +1,19 @@
 import type { SVGProps } from "react";
 
-export function CheckIcon(props: Readonly<SVGProps<SVGSVGElement>>) {
+import { cn } from "@/lib/utils";
+
+export function CheckIcon({
+  className,
+  ...props
+}: Readonly<SVGProps<SVGSVGElement>>) {
   return (
     <svg
-      className="h-3 w-3 text-white"
+      className={cn("h-3 w-3 text-white", className)}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
+      {...props}
     >
       <path
         d="M20 6L9 17L4 12"
